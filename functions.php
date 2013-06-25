c3758b5e75ba0bc577b7c442f5ff28bac5ac8366
Morten Brunbjerg Bech <bech@rephlex.dk>
2013-06-25 12:20:13 +0200

    Added Flexslider jquery plugin.

--- functions.php
+++ functions.php
@@ -177,6 +177,8 @@ function infoscreen_scripts() {
 // 	wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	
+	wp_enqueue_script( 'plugins', get_template_directory_uri() . '/js/plugins.jquery.js', array( 'jquery' ), '20130625' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
