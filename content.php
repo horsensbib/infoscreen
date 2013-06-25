edf40fe3a093ce2ba1e5a28dafaa5f346e15e184
Morten Brunbjerg Bech <bech@rephlex.dk>
2013-06-25 16:55:00 +0200

    Flexslider now works. Scrollbar now hidden in Firefox.

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
  * @since InfoScreen 1.0
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('slide'); ?> style="background-image:url('<?php infoscreen_img_src('slide-img'); ?>');">
+<li id="post-<?php the_ID(); ?>" <?php post_class('slide'); ?> style="background-image:url('<?php infoscreen_img_src('slide-img'); ?>');">
 	<div class="slide-content <?php 
 	$options = get_option('infoscreen_theme_options');
 	for ($i = 0; $i < $options['colorschemes']; $i += 2){
@@ -16,11 +16,9 @@
 	}
 		echo get_post_meta(get_the_ID(), '_infoscreen_layout', true). '"';
 		$transparency_value = get_post_meta(get_the_ID(), '_infoscreen_transparency', true)/100;
-		echo " style='color: rgb($font); background: rgba($bg,".$transparency_value.")"; ?>
-		'>
-		<header 
-		
-		class="entry-header">
+		echo 'style="color: rgb('.$font.'); background: rgba('.$bg.','.$transparency_value.');">';
+		?>
+		<header class="entry-header">
 		<h1 class="entry-title">
 			<?php the_title(); ?>
 		</h1>
@@ -36,6 +34,5 @@
 	</div>
 	<!-- .slide-content -->
 	<?php //edit_post_link( __( 'Edit', 'infoscreen' ), '<span class="edit-link">', '</span>' ); ?>
-</article>
+</li>
 <!-- #post-## -->
-<?php
--- functions.php
+++ functions.php
@@ -183,7 +183,7 @@ function infoscreen_scripts() {
 
 	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 	
-	wp_enqueue_script( 'plugins', get_template_directory_uri() . '/js/plugins.jquery.js', array( 'jquery' ), '20130625' );
+	wp_enqueue_script( 'plugins', get_template_directory_uri() . '/js/jquery.plugins.js', array( 'jquery' ), '20130625' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -194,6 +194,21 @@ function infoscreen_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'infoscreen_scripts' );
+
+
+function slideshow() {
+?>
+<script type="text/javascript">
+	jQuery(document).ready(function($){
+		$('.flexslider').flexslider({
+			animation: "slide",
+			controlNav: "false"
+		});
+	});
+</script>
+<?php
+}
+add_action('wp_footer', 'slideshow');
 
 /**
  * Implement the Custom Header feature
