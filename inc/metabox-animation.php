54c2cf69e3e487d834269ddb342490efc90c6390
Jacob Hansen <jbjh@horsens.dk>
2013-07-02 14:21:56 +0200

    Metabox animation default value changed

--- inc/metabox-animation.php
+++ inc/metabox-animation.php
@@ -21,20 +21,17 @@ function infoscreen_create_animation_metabox() {
 function animation_metabox() {
 	$currentvalue = get_post_meta(get_the_ID(), '_infoscreen_animation', true);
 	if($currentvalue == null){
-		$currentvalue = "left_slide_in";
+		$currentvalue = "fade";
 	}
 	echo "<fieldset>";
-	echo "<input type='radio' name='_infoscreen_animation' value='left_slide_in'";
-	echo ($currentvalue == 'left_slide_in')? 'checked="checked"':'';
-	echo "/>Slide in from left<br />";
+	echo "<input type='radio' name='_infoscreen_animation' value='slide'";
+	echo ($currentvalue == 'slide')? 'checked="checked"':'';
+	echo "/>Slide<br />";
 	
-	echo "<input type='radio' name='_infoscreen_animation' value='right_slide_in'";
-	echo ($currentvalue == 'right_slide_in')? 'checked="checked"':'';
-	echo "/>Slide in from right<br />";
+	echo "<input type='radio' name='_infoscreen_animation' value='fade'";
+	echo ($currentvalue == 'fade')? 'checked="checked"':'';
+	echo "/>Fade<br />";
 	
-	echo "<input type='radio' name='_infoscreen_animation' value='desolve'";
-	echo ($currentvalue == 'desolve')? 'checked="checked"':'';
-	echo "/>Desolve<br />";	
 	?>
 <input
 	type="hidden" name="infoscreen_animation_noncename"
@@ -46,14 +43,25 @@ function animation_metabox() {
 
 /* When the post is saved, saves our custom data */
 function infoscreen_animation_save_postdata( $post_id ) {
-
+	global $temp_post;
+	$cat_ID = wp_get_post_categories($post_id);
+	$args = array( 'category' => $cat_ID[0]);
+	$myposts = get_posts($args);
+	$data = $_POST['_infoscreen_animation'];
+	foreach( $myposts as $temp_post ) : setup_postdata($temp_post);
+		$temp_post_cat = wp_get_post_categories($temp_post->ID);
+		if(in_array($cat_ID[0], $temp_post_cat)){
+			$temp_post_id = $temp_post->ID;
+			update_post_meta($temp_post_id, '_infoscreen_animation', $data);
+		}
+	endforeach;
 	// verify this came from the our screen and with proper authorization,
 	// because save_post can be triggered at other animations
 	if ( !current_user_can( 'edit_post', $post_id ))
 		return $post_id;
 
 	// OK, we're authenticated: we need to find and save the data
-	$data = $_POST['_infoscreen_animation'];
+	
 	if(get_post_meta($post_id, '_infoscreen_animation') == '')
 		add_post_meta($post_id, '_infoscreen_animation', $data, true);
 
