973abdfe6d345cda3d4a7950eedd9fc6491a778d
Jacob Hansen <jbjh@horsens.dk>
2013-07-04 13:24:54 +0200

    Bugfix: Logo upload
    Now works as intended.

--- inc/script-upload.js
+++ inc/script-upload.js
@@ -1,52 +1,30 @@
 jQuery(document).ready(function($){
-// Uploading files
-var file_frame;
-var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
-var set_to_post_id = 10; // Set this
+    var custom_uploader;
  
-  jQuery('#upload_logo_button').live('click', function( event ){
+    $('#upload_logo_button').click(function(e) {
  
-    event.preventDefault();
+        e.preventDefault();
  
-    // If the media frame already exists, reopen it.
-    if ( file_frame ) {
-      // Set the post ID to what we want
-      file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
-      // Open frame
-      file_frame.open();
-      return;
-    } else {
-      // Set the wp.media post id so the uploader grabs the ID we want when initialised
-      wp.media.model.settings.post.id = set_to_post_id;
-    }
- 
-    // Create the media frame.
-    file_frame = wp.media.frames.file_frame = wp.media({
-      title: jQuery( this ).data( 'uploader_title' ),
-      button: {
-        text: jQuery( this ).data( 'uploader_button_text' ),
-      },
-      multiple: false  // Set to true to allow multiple files to be selected
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Choose Image',
+            button: {
+                text: 'Choose Image'
+            },
+            multiple: false
+        });
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            attachment = custom_uploader.state().get('selection').first().toJSON();
+            $('#preview_placeholder').attr('src',attachment.url);
+            $('#logo_url').val(attachment.url);
+        });
+        //Open the uploader dialog
+        custom_uploader.open();
     });
- 
-    // When an image is selected, run a callback.
-    file_frame.on( 'select', function() {
-      // We set multiple to false so only get one image from the uploader
-      attachment = file_frame.state().get('selection').first().toJSON();
- 
-      // Do something with attachment.id and/or attachment.url here
-      jQuery('#preview_placeholder').attr('src', attachment.url);
-      jQuery('#logo_url').val(attachment.url);
-      // Restore the main post ID
-      wp.media.model.settings.post.id = wp_media_post_id;
-    });
- 
-    // Finally, open the modal
-   file_frame.open();
-  });
-  
-  // Restore the main ID when the add media button is pressed
-  jQuery('a.add_media').on('click', function() {
-    wp.media.model.settings.post.id = wp_media_post_id;
-  });
 });
