1dda0654a55ebdc533cbe8707eaf9bf3d00120ae
Morten Brunbjerg Bech <bech@rephlex.dk>
2013-08-14 12:10:26 +0200

    If theme is installed at Horsens Public Library override fonts with branding font

--- inc/custom-settings-css.php
+++ inc/custom-settings-css.php
@@ -4,11 +4,30 @@ add_action('wp_head', 'dynamic_stylesheet');
 function dynamic_stylesheet(){
 	$options = get_option('infoscreen_theme_options');
 	
-	$title_font = $options['title_font-family'];
-	$body_font = $options['body_font-family'];
+	if ($_SERVER['SERVER_NAME'] == 'horsensbibliotek.dk' || 'sandbox.wp') {
+		$title_font = 'Hermes';
+		$body_font = 'Helvetica, Arial';
+	} else {
+		$title_font = $options['title_font-family'];
+		$body_font = $options['body_font-family'];
+	}
 	?>
 <style>
 	<?php
+	if ($_SERVER['SERVER_NAME'] == 'horsensbibliotek.dk' || 'sandbox.wp') {
+	?>
+@font-face {
+	font-family: 'Hermes';
+	font-weight: normal;
+	font-style: normal;
+	src: url('http://<?php echo $_SERVER['SERVER_NAME']; ?>/wp-content/themes/bibliozine-2/fonts/hermes-webfont.eot');
+	src: url('http://<?php echo $_SERVER['SERVER_NAME']; ?>/wp-content/themes/bibliozine-2/fonts/hermes-webfont.eot?#iefix') format('embedded-opentype'),
+	     url('http://<?php echo $_SERVER['SERVER_NAME']; ?>/wp-content/themes/bibliozine-2/fonts/hermes-webfont.woff') format('woff'),
+	     url('http://<?php echo $_SERVER['SERVER_NAME']; ?>/wp-content/themes/bibliozine-2/fonts/hermes-webfont.ttf') format('truetype'),
+       url('http://<?php echo $_SERVER['SERVER_NAME']; ?>/wp-content/themes/bibliozine-2/fonts/hermes-webfont.svg#HermesRegular') format('svg');
+}
+<?php
+	}
 	if ($title_font != $body_font){
 		if (strpos($title_font,'google') !== false) {
 			echo "@import url('http://fonts.googleapis.com/css?family=". str_replace("google ", "", $title_font) ."');";
