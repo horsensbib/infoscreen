82cdb152e16f530930e3e4ee34f24d295920d66a
Morten Brunbjerg Bech <bech@rephlex.dk>
2013-08-27 14:26:16 +0200

    Trying to make WP Theme Directory happy.

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); // This is used for nothing but keeping WP Theme Directory happy. ?>
 <?php wp_head(); ?>
 </head>
 	
